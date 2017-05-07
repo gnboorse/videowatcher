c1670c8a85706374e4e362dacda9334793498ae5
Gabriel Boorse <gnboorse@gmail.com>
2017-05-07 15:14:16 -0400

    Updated setup.py

--- setup.py
+++ setup.py
@@ -6,6 +6,9 @@ from setuptools import setup
 setup(
     name = 'videowatcher',
     packages = ['videowatcher'],
+    version = '1.0.0',
+    author = 'Gabriel Boorse',
+    description = 'Shared video playback package',
     include_package_data = True,
     install_requires = [
         'flask',
